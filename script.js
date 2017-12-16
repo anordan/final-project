738865f01eadd3b8de75a0c27b5ae24f870b7361
ashley nordan <abuxbaum@mcad.edu>
2017-12-15 19:37:26 -0600

    wk 15
    final project

---
+++ script.js
@@ -0,0 +1,174 @@
+  
+  // textillate to enhance the h1's on each page
+    $(document).ready(function() {
+	  console.log("javascriptloaded")
+	  $(".fancy_title").textillate();
+	});
+	  
+
+	  
+	  
+	//  ux page: fade toggles to make content appear under each sections title 
+
+    $(".readUx").fadeTo(1000, 0.7).fadeTo(800, 0.3).fadeTo(600, 1.0);
+	  
+	$(".uxContent").hide();
+	  
+    $(".readUx").click(function() {
+      console.log("clicky");
+      $(".uxContent").fadeToggle("slow");
+    });
+  
+    $(".readSpace").fadeTo(1000, 0.7).fadeTo(800, 0.3).fadeTo(600, 1.0);
+	  
+	$(".spaceContent").hide();
+	  
+	$(".readSpace").click(function() {
+	  console.log("clicky");
+      $(".spaceContent").fadeToggle("slow");
+    });
+  
+    $(".readColor").fadeTo(1000, 0.7).fadeTo(800, 0.3).fadeTo(600, 1.0);
+	
+	$(".colorContent").hide();
+	  
+	$(".readColor").click(function() {
+	  console.log("clicky");
+    $(".colorContent").fadeToggle("slow");
+    });
+  
+    $(".readStyle").fadeTo(1000, 0.7).fadeTo(800, 0.3).fadeTo(600, 1.0);
+	  
+	$(".styleContent").hide();
+	  
+	$(".readStyle").click(function() {
+	  console.log("clicky");
+    $(".styleContent").fadeToggle("slow");
+    });
+ 
+ 
+    $(".readApp").fadeTo(1000, 0.7).fadeTo(800, 0.3).fadeTo(600, 1.0);
+	  
+	$(".appContent").hide();
+	  
+	$(".readApp").click(function() {
+	  console.log("clicky");
+    $(".appContent").fadeToggle("slow");
+    });
+  
+    $(".readConclusion").fadeTo(1000, 0.7).fadeTo(800, 0.3).fadeTo(600, 1.0);
+	  
+	$(".conclusionContent").hide();
+	  
+	$(".readConclusion").click(function() {
+		  console.log("clicky");
+    $(".conclusionContent").fadeToggle("slow");
+    });
+  
+  
+  // color page: fade toggles to make content appear under each sections title  
+  
+    $(".readBase").fadeTo(1000, 0.7).fadeTo(800, 0.3).fadeTo(600, 1.0);
+	  
+	$(".baseContent").hide();
+	  
+	$(".readBase").click(function() {
+	  console.log("clicky");
+    $(".baseContent").fadeToggle("slow");
+    });
+  
+    $(".readChoose").fadeTo(1000, 0.7).fadeTo(800, 0.3).fadeTo(600, 1.0);
+	  
+	$(".chooseContent").hide();
+	  
+	$(".readChoose").click(function() {
+	  console.log("clicky");
+    $(".chooseContent").fadeToggle("slow");
+    });
+  
+    $(".readNice").fadeTo(1000, 0.7).fadeTo(800, 0.3).fadeTo(600, 1.0);
+	  
+	$(".niceContent").hide();
+	  
+	$(".readNice").click(function() {
+	  console.log("clicky");
+    $(".niceContent").fadeToggle("slow");
+    });
+  
+    $(".readPalette").fadeTo(1000, 0.7).fadeTo(800, 0.3).fadeTo(600, 1.0);
+	  
+	$(".paletteContent").hide();
+	  
+	$(".readPalette").click(function() {
+	  console.log("clicky");
+    $(".paletteContent").fadeToggle("slow");
+    });
+  
+    $(".readAccent").fadeTo(1000, 0.7).fadeTo(800, 0.3).fadeTo(600, 1.0);
+	  
+	$(".accentContent").hide();
+	  
+	$(".readAccent").click(function() {
+	  console.log("clicky";
+    $(".accentContent").fadeToggle("slow");
+    });
+  
+    $(".readGray").fadeTo(1000, 0.7).fadeTo(800, 0.3).fadeTo(600, 1.0);
+	  
+	$(".grayContent").hide();
+	  
+	$(".readGray").click(function() {
+	  console.log("clicky");
+    $(".grayContent").fadeToggle("slow");
+    });
+  
+    $(".readCreate").fadeTo(1000, 0.7).fadeTo(800, 0.3).fadeTo(600, 1.0);
+	  
+	$(".createContent").hide();
+	  
+	$(".readCreate").click(function() {
+	  console.log("clicky");
+    $(".createContent").fadeToggle("slow");
+    });
+  
+    $(".readScheme").fadeTo(1000, 0.7).fadeTo(800, 0.3).fadeTo(600, 1.0);
+	  
+	$(".schemeContent").hide();
+	  
+	$(".readScheme").click(function() {
+	  console.log("clicky");
+    $(".schemeContent").fadeToggle("slow");
+    });
+  
+    $(".readAction").fadeTo(1000, 0.7).fadeTo(800, 0.3).fadeTo(600, 1.0);
+	  
+	$(".actionContent").hide();
+	  
+	$(".readAction").click(function() {
+	  console.log("clicky");
+    $(".actionContent").fadeToggle("slow");
+    });
+  
+    $(".readConclusion").fadeTo(1000, 0.7).fadeTo(800, 0.3).fadeTo(600, 1.0);
+	  
+	$(".finalContent").hide();
+	  
+	$(".readFinal").click(function() {
+	  console.log("clicky");
+    $(".finalContent").fadeToggle("slow");
+    });
+
+  
+  
+  
+  
+  
+ 
+  
+	  
+  
+  
+  
+  
+  
+  
